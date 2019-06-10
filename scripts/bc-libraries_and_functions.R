b9a88312ddb4d543b7da9bb6e27354e5670005e3
Alexandra Paxton <paxton.alexandra@gmail.com>
2019-06-10 15:53:27 -0400

    Add libraries and functions file

---
+++ scripts/bc-libraries_and_functions.R
@@ -0,0 +1,36 @@
+#### bc-libraries_and_functions.r: Part of `beyond_consistency.Rmd` ####
+#
+# This script loads libraries and creates a number of 
+# additional functions to facilitate data prep and analysis.
+#
+# Written by: A. Paxton (University of Connecticut)
+# Date last modified: 10 June 2019
+#####################################################################################
+
+#### Load necessary packages ####
+
+# list of required packages
+required_packages = c(
+  'knitr',
+  'dplyr',
+  'lme4',
+  'ggplot2',
+  'pander',
+  'viridis',
+  'tidyr',
+  'tibble',
+  'RCurl',
+  'lmerTest',
+  'crqa'
+)
+
+# load required packages using pacman
+pacman::p_load(required_packages, character.only=TRUE)
+
+#### Prevent scientific notation ####
+options(scipen=999)
+
+# read in pander_lme
+pander_lme_url = "https://raw.githubusercontent.com/a-paxton/stats-tools/2a1bf715097bbcc966ab612af3a9e0b14408d4ff/pander_lme.R"
+pander_lme_file = getURL(pander_lme_url, ssl.verifypeer = FALSE)
+eval(parse(text = pander_lme_file))
